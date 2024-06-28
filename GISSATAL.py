072ea6d59d0e62ba2a071b00c85837c0210f866f
tomas1978 <tomas.nilsson78@gmail.com>
2024-06-28 07:36:44 +0200

    Couning number of guesses

--- GISSATAL.py
+++ GISSATAL.py
@@ -1,7 +1,9 @@
 import random 
+gissningar=0
 slumptal=random.randint(0,101)
 tal=-1
 while tal!=slumptal:
+  gissningar+=1
   tal=int(input("Gissa tal 0-100: "))
   if tal<slumptal:
     print("for lagt")
@@ -9,6 +11,7 @@ while tal!=slumptal:
     print("for hogt")
   else:
     print("bra! Ratt gissning")
+    print("Antal gissningar",gissningar)
     break
     
 
